d57b5e2931aee31b2760b109fe3ea2b54b518790
Bunty Madan <mail.bunty.m@gmail.com>
2019-08-20 14:14:35 +0800

    Xcode project template.

---
+++ Weather/Weather/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by bunty.madan on 20/8/19.
+//  Copyright © 2019 Bunty Madan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
